4afc08e525e3be532329bd28ce93254c1d466861
Li, Yifei <realliyifei@outlook.com>
2022-06-07 11:32:20 -0400

    activate USE_NEGC

--- src/transformers/models/bert/modeling_bert.py
+++ src/transformers/models/bert/modeling_bert.py
@@ -108,8 +108,8 @@ BERT_PRETRAINED_MODEL_ARCHIVE_LIST = [
 
 #Add by Yifei: load conceptor matrix, negc, for the layers of bert models (bert-base-uncased, bert-tiny)
 PRINT_NEGC_INTERMEDIATE = False  #Print each layer's output 
-USE_NEGC = False #Use negc in each layer
-USE_POST_PROCESS = True #Use negc in the last layer
+USE_NEGC = True #Use negc in each layer
+USE_POST_PROCESS = False #Use negc in the last layer
 assert not (USE_NEGC and USE_POST_PROCESS) 
 
 import pickle
